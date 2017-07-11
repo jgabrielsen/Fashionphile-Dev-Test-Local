4265a8efb4eb085da2c4e378c060ba6b1eb32375
James Gabrielsen <jgabrielsen@gmail.com>
2017-07-11 00:12:37 -0600

    no message

--- js/callProductData.js
+++ js/callProductData.js
@@ -1,36 +1,36 @@
 var url = "../json/productData.json";
 
-$.ajax({
-    url: url,
-    dataType: 'JSONP',
-    type: 'GET',
-    success: function (data) {
-        console.log(data);
-    }
-});
+// $.ajax({
+//     url: url,
+//     dataType: 'JSONP',
+//     jsonpCallback: 'callback',
+//     type: 'GET',
+//     success: function (data) {
+//         console.log(data);
+//     }
+// });
 
-// var dataDump = $.getJSON( url, function() {
-// 	var productObjects = dataDump.responseJSON.data;
-// 	console.log(productObjects);
-// 	$.each( productObjects, function( i, product ) {
-// 		//console.log(productObject);
-// 		$(".products").append(
-// 			"<li class='four columns product'>" +
-// 			'<img class="product-image" src="http://www.fashionphile.com/'+product.images+'" alt="'+product.name+'" onError="this.onerror=null;this.src=\'images/noimage.png\';" />'+
-// 			'<div class="product-info">'+
-// 	        '<p class="product-name">'+product.name+'</p>'+
-//         	'<p class="product-brand">'+product.brand+'</p>'+
-//         	'</div>'+
-// 			"</li>"
-// 		)
-// 	});
-// })
-//   .done(function() {
-// 	console.log( "Call to product JSON successful" );
-//   })
-//   .fail(function() {
-// 	console.log( "Unable to call product JSON" );
-//   })
-//   .always(function() {
-// 	console.log( "Call complete" );
-//   });
+var dataDump = $.getJSON( url, function() {
+	var productObjects = dataDump.responseJSON.data;
+	console.log(productObjects);
+	$.each( productObjects, function( i, product ) {
+		$(".products").append(
+			"<li class='four columns product'>" +
+			'<img class="product-image" src="http://www.fashionphile.com/'+product.images+'" alt="'+product.name+'" onError="this.onerror=null;this.src=\'images/noimage.png\';" />'+
+			'<div class="product-info">'+
+	        '<p class="product-name">'+product.name+'</p>'+
+        	'<p class="product-brand">'+product.brand+'</p>'+
+        	'</div>'+
+			"</li>"
+		)
+	});
+})
+  .done(function() {
+	console.log( "Call to product JSON successful" );
+  })
+  .fail(function() {
+	console.log( "Unable to call product JSON" );
+  })
+  .always(function() {
+	console.log( "Call complete" );
+  });
